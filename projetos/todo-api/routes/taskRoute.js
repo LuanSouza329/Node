7547a63c368840a26949404d62bdf4cd985409d0
Luan <luansouza329@gmail.com>
2026-01-11 13:09:42 -0300

    Adição da documentação swagger para os patchs, descrição, status e titulo

--- projetos/todo-api/routes/taskRoute.js
+++ projetos/todo-api/routes/taskRoute.js
@@ -223,6 +223,58 @@ router.put(
   TaskController.updateTask
 );
 
+/**
+ * @swagger
+ * /api/task/update/title/{id}:
+ *   patch:
+ *     summary: Atualiza o título de uma task existente
+ *     tags: [Task]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: O titulo será atualizado
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               titulo:
+ *                 type: string
+ *                 description: O titulo será atualizado
+ *                 example: Estudar MySQL
+ *     responses:
+ *       200:
+ *         description: Titulo atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Titulo atualizado com sucesso!
+ *                 task:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 1
+ *                     titulo:
+ *                       type: string
+ *                       example: Estudar MySQL
+ *       400:
+ *         description: Título inválido ou incompleto
+ *       404:
+ *         description: Usuário não encontrado
+ *       500:
+ *         description: Erro interno do servidor
+ */
+
 router.patch(
   "/task/update/title/:id",
 
@@ -236,6 +288,59 @@ router.patch(
   TaskController.updateTitle
 );
 
+
+/**
+ * @swagger
+ * /api/task/update/description/{id}:
+ *   patch:
+ *     summary: Atualiza a descrição de uma task existente
+ *     tags: [Task]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: A descrição será atualizada
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               descricao:
+ *                 type: string
+ *                 description: Descrição da task será atualizada
+ *                 example: Estudar MySql no site https://www.w3schools.com/mysql/default.asp
+ *     responses:
+ *       200:
+ *         description: Descrição atualizada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Descrição atualizada com sucesso!
+ *                 task:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 1
+ *                     descricao:
+ *                       type: string
+ *                       example: Estudar MySql no site https://www.w3schools.com/mysql/default.asp
+ *       400:
+ *         description: Descricao inválida ou incompleta
+ *       404:
+ *         description: Usuário não encontrado
+ *       500:
+ *         description: Erro interno do servidor
+ */
+
 router.patch(
   "/task/update/description/:id",
 
@@ -248,6 +353,60 @@ router.patch(
   validate,
   TaskController.updateDescription
 );
+
+
+/**
+ * @swagger
+ * /api/task/update/status/{id}:
+ *   patch:
+ *     summary: Atualiza o status de uma task existente com 2 estados pendente / concluida
+ *     tags: [Task]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: O status será atualizado
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               status:
+ *                 type: string
+ *                 description: O status da task será atualizado
+ *                 example: concluida
+ *     responses:
+ *       200:
+ *         description: Status atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Status atualizada com sucesso!
+ *                 task:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 1
+ *                     status:
+ *                       type: string
+ *                       example: concluida
+ *       400:
+ *         description: Status inválido ou incompleto
+ *       404:
+ *         description: Usuário não encontrado
+ *       500:
+ *         description: Erro interno do servidor
+ */
+
 
 router.patch(
   "/task/update/status/:id",
